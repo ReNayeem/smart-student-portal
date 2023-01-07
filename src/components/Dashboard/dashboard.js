0cea800241158877dc2acbdc5bcd4cea07cda183
ReNayeem <renayeem@gmail.com>
2023-01-07 21:05:58 +0600

    working on dashboard

--- src/components/Dashboard/dashboard.js
+++ src/components/Dashboard/dashboard.js
@@ -5,8 +5,49 @@ const Dashboard = () => {
     return (
         <div className='custom-container'>
             <div className='custom-padding'>
-                <p className='pt-4 hi-dashboard'>Hi Nayeem</p>
-                <h1 className='welcome-dashboard'>Welcome Dashboard</h1>
+
+                <div className='d-flex'>
+                    <div className='custom-width'>
+
+
+                        <h1 className='pt-4 welcome-dashboard'>My Dashboard</h1>
+                        <p className='hi-dashboard'>Welcome back Nayeem, ready for another day of your UNI life?🔥</p>
+
+
+                        <div class="flip-card mt-3">
+                            <div class="flip-card-inner">
+                                <div class="flip-card-front">
+                                    <p class="title-card">FLIP CARD</p>
+                                    <p>Hover Me</p>
+                                </div>
+                                <div class="flip-card-back">
+                                    <p class="title-card">BACK</p>
+                                    <p>Leave Me</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <h3 className='pt-4 welcome-dashboard'>Upcoming</h3>
+                        <div className='d-flex mt-3'>
+                            <div className='me-4 d-flex align-items-center'>
+                                <h3>Payment</h3>
+                            </div>
+                            <div class="flip-card">
+                                <div class="flip-card-inner">
+                                    <div class="flip-card-front">
+                                        <p class="title-card mt-3">29,872 TK</p>
+                                        <p>Hover</p>
+                                    </div>
+                                    <div class="flip-card-back">
+                                        <p class="title-card mt-3">Mid-Term</p>
+                                        <p>22/2/23</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
