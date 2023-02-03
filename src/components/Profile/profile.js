43e40a5b5c30e4990a3cbc9c2898f2acd12d76f5
Nayeam Ahmed <renayeem@gmail.com>
2023-02-03 14:37:39 +0600

    profile address and ratings added

--- src/components/Profile/profile.js
+++ src/components/Profile/profile.js
@@ -12,10 +12,14 @@ const profile = () => {
                             <img className='profile-img-2' src={profileImg} alt="" />
 
                             <div>
-                                <h2>Md. Nayeem</h2>
+                                <div className='d-flex justify-content-between profile-name'>
+                                    <h2>Md. Nayeem</h2>
+                                    <h4><i class="fa-solid fa-location-dot"></i><span>Narayanganj, Dhaka</span></h4>
+                                </div>
                                 <h5>B.Sc. at CSE</h5>
 
-                                <p>OVERALL TERM RANKINGS</p>
+                                <p>OVERALL TERM RATINGS</p>
+                                <h3>8,2 <span><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i className="fa-regular rating fa-star"></i></span></h3>
                             </div>
                         </div>
 
