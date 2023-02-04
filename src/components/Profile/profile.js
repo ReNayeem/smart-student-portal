eae6e3be679dcf1c773b548de9606959f616c9cc
Nayeam Ahmed <renayeem@gmail.com>
2023-02-04 18:44:21 +0600

    profile page done

--- src/components/Profile/profile.js
+++ src/components/Profile/profile.js
@@ -24,11 +24,81 @@ const profile = () => {
                                 <p>OVERALL TERM RATINGS</p>
                                 <h3>8,2 <span><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i className="fa-regular rating fa-star"></i></span></h3>
 
-                                <button>UPDATE PROFILE</button>
+                                <div className='d-flex'>
+                                    <a target='_blank' href="https://t.me/FateNayeam"><i class="fa-solid fa-message"></i>Send message</a>
+                                    <button>UPDATE PROFILE</button>
+                                </div>
                             </div>
                         </div>
 
-                        <div className='profile-second-section'></div>
+                        <hr className='profile-hr' />
+
+                        <div className='profile-second-section'>
+                            <div className='profile-second-first-section'>
+                                <div>
+                                    <h5>PROGRAM</h5>
+                                    <p>Bachelor of Science (B.Sc.) 4-Year</p>
+                                    <p>Computer Science and Engineering</p>
+                                </div>
+
+                                <div>
+                                    <h5 className='mt-4'>CAMPUS</h5>
+                                    <p>Permanent Campus</p>
+                                </div>
+
+                                <hr className='profile-hr-2' />
+
+                                <div>
+                                    <div className='d-flex mt-4'>
+                                        <h5>CURRENT ADDRESS</h5>
+                                        <button>Primary</button>
+                                    </div>
+                                    <p>40/4 XYZ, Narayanganj</p>
+                                </div>
+
+                                <div>
+                                    <div className='d-flex mt-4'>
+                                        <h5>PERMANENT ADDRESS</h5>
+                                        <button>Secondary</button>
+                                    </div>
+                                    <p>XYZ, Bondor, Narayanganj</p>
+                                </div>
+
+                            </div>
+
+                            <div className='profile-second-second-section'>
+                                <div className='virtual-contact-information'>
+                                    <p>VIRTUAL CONTACT INFORMATION</p>
+                                    <div className='d-flex'>
+                                        <h5 className='profile-text-width'>Phone: </h5>
+                                        <h5>+8801794354468</h5>
+                                    </div>
+                                    <div className='d-flex'>
+                                        <h5 className='profile-text-width'>E-mail: </h5>
+                                        <h5>nayeem15-13434@diu.edu.bd</h5>
+                                    </div>
+                                    <div className='d-flex'>
+                                        <h5 className='profile-text-width'>Site: </h5>
+                                        <a href="https://renayeem.netlify.com" target='_blank'>renayeem.netlify.com</a>
+                                    </div>
+                                </div>
+
+                                <hr className='profile-hr-2' />
+
+                                <div className='basic-information'>
+                                    <p>BASIC INFORMATION</p>
+                                    <div className='d-flex'>
+                                        <h5 className='profile-text-width'>Birthday: </h5>
+                                        <h5>January 22, 2001</h5>
+                                    </div>
+                                    <div className='d-flex'>
+                                        <h5 className='profile-text-width'>Gender: </h5>
+                                        <h5>Male</h5>
+                                    </div>
+                                    {/* <a href="https://google.com" target='_blank'>click</a> */}
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
