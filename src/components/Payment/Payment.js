ea8354895a7e6a99bf33bf838ecb95b9283df5ce
Nayeam Ahmed <renayeem@gmail.com>
2023-03-24 17:49:44 +0600

    payment first section basic design done

--- src/components/Payment/Payment.js
+++ src/components/Payment/Payment.js
@@ -10,24 +10,30 @@ const Payment = () => {
                         <div>
                             <h2 className='mt-1'>Payment Details</h2>
                             <div className='d-flex'>
-                                <div className='payment-first-first-section'>
+                                <div className='payment-first-first-section payment-first-section'>
                                     <i class="fa-solid fa-wallet"></i>
                                     <h5>Payable</h5>
                                     <h3>৳888888</h3>
                                 </div>
-                                <div className='payment-first-second-section'>
-                                    <h5>Total Paid</h5>
+                                <div className='payment-first-second-section payment-first-section'>
+                                    <i class="fa-solid fa-wallet"></i>
+                                    <h5>Paid</h5>
+                                    <h3>৳788888</h3>
                                 </div>
                             </div>
                         </div>
                         <div className='d-flex'>
-                            <div className='payment-first-third-section'>
+                            <div className='payment-first-third-section payment-first-section'>
+                                <i class="fa-solid fa-wallet"></i>
                                 <h5>Semester Due</h5>
+                                <h3>৳188888</h3>
                             </div>
                             <div>
-                                <h2>Upcoming Payment</h2>
-                                <div className='payment-first-fourth-section'>
-                                    <h5>UP</h5>
+                                <h2 className='mt-1 text-center'>Upcoming Payment</h2>
+                                <div className='payment-first-fourth-section payment-first-section'>
+                                    <i class="fa-solid fa-wallet"></i>
+                                    <h5>Upcoming Payment</h5>
+                                    <h3>৳88888</h3>
                                 </div>
                             </div>
                         </div>
