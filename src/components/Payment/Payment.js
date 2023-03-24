dade94630b06d0f539fa9de5d8b7efbb6c681c95
Nayeam Ahmed <renayeem@gmail.com>
2023-03-24 12:47:04 +0600

    payment first section divided

--- src/components/Payment/Payment.js
+++ src/components/Payment/Payment.js
@@ -3,8 +3,35 @@ import './Payment.css'
 
 const Payment = () => {
     return (
-        <div>
-            <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut voluptatibus deserunt fugiat, commodi et veniam repellendus fugit delectus. Ipsum quibusdam ullam voluptas fugiat impedit laudantium animi dignissimos. Laborum, odio recusandae.</h1>
+        <div className='custom-container payment'>
+            <div className='custom-padding custom-container2'>
+                <div className='custom-width'>
+                    <div className='payment-details d-flex'>
+                        <div>
+                            <h2>Payment Details</h2>
+                            <div className='d-flex'>
+                                <div className='payment-first-first-section'>
+                                    <h5>Total Payable</h5>
+                                </div>
+                                <div className='payment-first-second-section'>
+                                    <h5>Total Paid</h5>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='d-flex'>
+                            <div className='payment-first-third-section'>
+                                <h5>Semester Due</h5>
+                            </div>
+                            <div>
+                                <h2>Upcoming Payment</h2>
+                                <div className='payment-first-fourth-section'>
+                                    <h5>UP</h5>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
