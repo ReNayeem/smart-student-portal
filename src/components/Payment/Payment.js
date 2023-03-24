a99453e4623b81ccea511804a718b9488a9672b9
Nayeam Ahmed <renayeem@gmail.com>
2023-03-25 00:02:25 +0600

    data updated and upcoming payment rough completed

--- src/components/Payment/Payment.js
+++ src/components/Payment/Payment.js
@@ -13,27 +13,28 @@ const Payment = () => {
                                 <div className='payment-first-first-section payment-first-section'>
                                     <i class="fa-solid fa-wallet"></i>
                                     <h5>Payable</h5>
-                                    <h3>৳888888</h3>
+                                    <h3>৳781,020.00</h3>
                                 </div>
                                 <div className='payment-first-second-section payment-first-section'>
-                                    <i class="fa-solid fa-wallet"></i>
+                                    <i class="fa-solid fa-hand-holding-dollar"></i>
                                     <h5>Paid</h5>
-                                    <h3>৳788888</h3>
+                                    <h3>৳736,470.01</h3>
                                 </div>
                             </div>
                         </div>
                         <div className='d-flex'>
                             <div className='payment-first-third-section payment-first-section'>
-                                <i class="fa-solid fa-wallet"></i>
+                                <i class="fa-solid fa-piggy-bank"></i>
                                 <h5>Semester Due</h5>
-                                <h3>৳188888</h3>
+                                <h3>৳43,529.99</h3>
                             </div>
                             <div>
-                                <h2 className='mt-1 text-center'>Upcoming Payment</h2>
+                                <h2 className='mt-1 text-center upcoming-payment-text'>Upcoming Payment</h2>
                                 <div className='payment-first-fourth-section payment-first-section'>
-                                    <i class="fa-solid fa-wallet"></i>
+                                    <i class="fa-solid fa-money-check-dollar"></i>
                                     <h5>Upcoming Payment</h5>
-                                    <h3>৳88888</h3>
+                                    <h3 className='upcoming-payment'>৳21,805.49</h3>
+                                    <h5 className='upcoming-payment-last-date'>Last Date: 31 December</h5>
                                 </div>
                             </div>
                         </div>
